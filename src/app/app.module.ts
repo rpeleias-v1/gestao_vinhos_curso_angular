6bf9721066b8096a638b09b99ab994af69bbc0df
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-04-06 21:53:53 -0300

    Configurada AP simulada para operações REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { VinhosComponent } from './components/vinhos/vinhos.component';
+import { VinhosService } from './services/vinhos.service';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { VinhoApi } from './vinho-api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VinhosComponent    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule  ,
+    InMemoryWebApiModule.forRoot(VinhoApi)  
   ],
-  providers: [],
+  providers: [VinhosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
