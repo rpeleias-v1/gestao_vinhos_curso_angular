1814e65b22aa655ddb4c47c1d0404e2a0edea15f
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-04-07 23:46:36 -0300

    Fluxo básico: cadastro de vinhos

--- src/app/components/cadastro-vinho/cadastro-vinho.component.ts
+++ src/app/components/cadastro-vinho/cadastro-vinho.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+import { Vinho } from '../../models/vinho';
+
+import { VinhosService } from '../../services/vinhos.service';
+
 @Component({
   selector: 'cadastro-vinho',
   templateUrl: './cadastro-vinho.component.html',
@@ -9,13 +13,32 @@ import { Router } from '@angular/router';
 })
 export class CadastroVinhoComponent implements OnInit {
 
-  constructor(private router: Router){ }
+  vinho: Vinho;
+  uvas: Array<string>;
+  classificacoes: Array<string>;
+
+  constructor(private router: Router, private vinhoService: VinhosService){ }
 
   ngOnInit() {
+    this.vinho = new Vinho();
+    this.uvas = ['Merlot', 'Cabernet Sauvignon', 'Carmenere'];
+    this.classificacoes = ['Tinto', 'Branco', 'Verde'];
   }
 
   voltar(): void {
     this.router.navigate(['/vinhos']);
+  }
+
+  salvar() {
+    this.vinhoService.cadastrar(this.vinho)
+      .then(response => {
+        console.log(JSON.stringify(response));
+        alert("Vinho cadastrado com sucesso");
+        this.router.navigate(['/vinhos']);
+      })
+      .catch(erro => {
+        console.log(erro);
+      })
   }
 
 }
--- src/app/services/vinhos.service.ts
+++ src/app/services/vinhos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Vinho } from './../models/vinho';
 
-import { Http } from '@angular/http';
+import { Http, Request, RequestOptions, RequestOptionsArgs, Response, Headers } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -17,9 +17,22 @@ export class VinhosService {
       .then(response => response.json().data as Array<Vinho>)
       .catch(this.tratarErro);
   }
+  
+  cadastrar(vinho: Vinho): Promise<Response> {
+    return this.http.post('api/vinhos', JSON.stringify(vinho), this.header())
+      .toPromise()
+      .then(response => response)
+      .catch(this.tratarErro);
+  }
 
   private tratarErro(erro:any): Promise<any> {
     console.log(erro);
     return Promise.reject(erro.message | erro);
+  }
+
+  private header(): RequestOptions {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let requestOptions = new RequestOptions({ headers: headers });
+    return requestOptions;
   }
 }
