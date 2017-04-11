47e1c00e30279fda1680626e3584ce13f2b9b73e
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-04-10 22:21:51 -0300

    Atualização de vinhos com PUT

--- src/app/components/cadastro-vinho/cadastro-vinho.component.ts
+++ src/app/components/cadastro-vinho/cadastro-vinho.component.ts
@@ -47,10 +47,30 @@ export class CadastroVinhoComponent implements OnInit {
   }
 
   salvar() {
+    if(this.vinho.id) {
+      this.atualizar();      
+    } else {
+      this.cadastrarNovo();
+    }    
+  }
+
+  private cadastrarNovo() {
     this.vinhoService.cadastrar(this.vinho)
       .then(response => {
         console.log(JSON.stringify(response));
         alert("Vinho cadastrado com sucesso");
+        this.router.navigate(['/vinhos']);
+      })
+      .catch(erro => {
+        console.log(erro);
+      })
+  }
+
+  private atualizar() {
+    this.vinhoService.atualizar(this.vinho.id, this.vinho)
+      .then(response => {
+        console.log(JSON.stringify(response));
+        alert("Vinho atualizado com sucesso");
         this.router.navigate(['/vinhos']);
       })
       .catch(erro => {
