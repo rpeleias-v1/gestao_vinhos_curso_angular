b4c3dc2532428df5f94c674004677260777dafef
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-04-14 14:51:30 -0300

    Adicionado modulo de animação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -26,6 +27,7 @@ import { NotificacaoComponent } from './components/notificacao/notificacao.compo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule  ,
     InMemoryWebApiModule.forRoot(VinhoApi),
--- src/app/components/notificacao/notificacao.component.ts
+++ src/app/components/notificacao/notificacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Notificacao } from '../../models/notificacao';
 import { NotificacaoService} from '../../services/notificacao.service';
@@ -6,28 +7,39 @@ import { NotificacaoService} from '../../services/notificacao.service';
 @Component({
   selector: 'notificacao',
   templateUrl: './notificacao.component.html',
-  styleUrls: ['./notificacao.component.css']
+  styleUrls: ['./notificacao.component.css'],
+  animations: [
+    trigger('visivel', [
+      state('visivel', style({
+        opacity: 1
+      })),
+      state('naoVisivel', style({
+        opacity: 0
+      })),
+      transition( 'visivel => naoVisivel', animate('.5s'))
+    ])
+  ]
 })
 export class NotificacaoComponent implements OnInit {
 
   notificacao: Notificacao;
-  visivel: boolean  = false;
+  visivel: string  = 'naoVisivel';
 
   constructor(private notificacaoService: NotificacaoService) { }
 
   ngOnInit() {
     this.notificacaoService.obterNotificacoes().subscribe((notificacao: Notificacao) => {
       this.notificacao = notificacao;
-      this.visivel = true;
+      this.visivel = 'visivel';
       setTimeout(() => {
-        this.visivel = false;
+        this.visivel = 'naoVisivel';
       }, 2000);
     })
   }
 
   tipoAlerta() {
-    if (this.notificacao.tipo === 'success' || this.notificacao.tipo === 'warning' || 
-        this.notificacao.tipo === 'info' || this.notificacao.tipo === 'danger') {
+    if (this.notificacao &&  (this.notificacao.tipo === 'success' || this.notificacao.tipo === 'warning' || 
+        this.notificacao.tipo === 'info' || this.notificacao.tipo === 'danger')) {
       return `alert-${this.notificacao.tipo}`;
     }
     return 'alert-success';
