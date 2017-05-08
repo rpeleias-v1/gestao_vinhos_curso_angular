f7492e4bcecfcf4097e01dd1dd731bd9c4f836ea
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-05-07 21:09:51 -0300

    Componente de notificacão melhorado

--- src/app/components/notificacao/notificacao.component.ts
+++ src/app/components/notificacao/notificacao.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { state, trigger, style, transition, animate} from '@angular/animations';
+
 import { Notificacao } from '../../models/notificacao';
 import { NotificacaoService} from '../../services/notificacao.service';
 
 @Component({
   selector: 'notificacao',
   templateUrl: './notificacao.component.html',
-  styleUrls: ['./notificacao.component.css']
+  styleUrls: ['./notificacao.component.css'],
+  animations: [
+    trigger('visibilidade', [
+      state('visivel', style({
+        opacity: 1
+      })),
+      state('naoVisivel', style({
+        opacity: 0
+      })),
+      transition('visivel => naoVisivel', animate('.5s')),
+      transition('naoVisivel => visivel', animate('.5s'))
+    ])]  
 })
 export class NotificacaoComponent implements OnInit {
 
   notificacao: Notificacao;
-  visivel: boolean  = false;
+  status: string  = 'naoVisivel';
 
   constructor(private notificacaoService: NotificacaoService) { }
 
   ngOnInit() {
     this.notificacaoService.obterNotificacoes().subscribe((notificacao: Notificacao) => {
       this.notificacao = notificacao;
-      this.visivel = true;
+      this.status = 'visivel';
       setTimeout(() => {
-        this.visivel = false;
+        this.status = 'naoVisivel';
       }, 2000);
     })
   }
